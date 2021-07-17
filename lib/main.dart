bb942cf6e347065a3c99b401f88ef6e2a14a7342
Airton Araujo <airton.araujo.s18@gmail.com>
2021-07-17 17:12:07 -0300

    splach screen with AnimatedCard

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,12 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: SplashPage(),
--- lib/src/views/splash/splash_page.dart
+++ lib/src/views/splash/splash_page.dart
@@ -1,3 +1,4 @@
+import 'package:animated_card/animated_card.dart';
 import 'package:bloco_de_notas/src/shared/constants/app_colors.dart';
 import 'package:bloco_de_notas/src/shared/constants/text_styles.dart';
 import 'package:flutter/material.dart';
@@ -17,54 +18,62 @@ class _SplashPageState extends State<SplashPage> {
           decoration: BoxDecoration(gradient: AppColors.blueGradient),
           child: Stack(
             children: [
-              Stack(
-                alignment: Alignment.center,
-                children: [
-                  Positioned(
-                      top: (MediaQuery.of(context).size.height / 2) - 100,
-                      child: Hero(
-                        child: Image.asset('assets/images/notes_logo.png'),
-                        tag: 'notes_logo',
-                      )),
-                  Center(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Hero(
-                          tag: "journal",
-                          child: Material(
-                            color: Colors.transparent,
-                            child: Text(
-                              "journal",
-                              style: TextStyles.white72w700Montserrat,
+              AnimatedCard(
+                direction: AnimatedCardDirection.top,
+                duration: Duration(seconds: 1),
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Positioned(
+                        top: (MediaQuery.of(context).size.height / 2) - 100,
+                        child: Hero(
+                          child: Image.asset('assets/images/notes_logo.png'),
+                          tag: 'notes_logo',
+                        )),
+                    Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Hero(
+                            tag: "journal",
+                            child: Material(
+                              color: Colors.transparent,
+                              child: Text(
+                                "journal",
+                                style: TextStyles.white72w700Montserrat,
+                              ),
                             ),
                           ),
-                        ),
-                        Text(
-                          "YOUR ACADEMY'S BEST FRIEND",
-                          style: TextStyles.white13w300Montserrat,
-                          textAlign: TextAlign.end,
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      "powered by",
-                      style: TextStyles.white12w200Roboto,
-                    ),
-                    SizedBox(
-                      height: 8.0,
-                    ),
-                    Image.asset("assets/images/raro_academy_logo.png"),
-                    SizedBox(height: 40.0)
+                          Text(
+                            "YOUR ACADEMY'S BEST FRIEND",
+                            style: TextStyles.white13w300Montserrat,
+                            textAlign: TextAlign.end,
+                          ),
+                        ],
+                      ),
+                    )
                   ],
+                ),
+              ),
+              AnimatedCard(
+                direction: AnimatedCardDirection.bottom,
+                duration: Duration(seconds: 1),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "powered by",
+                        style: TextStyles.white12w200Roboto,
+                      ),
+                      SizedBox(
+                        height: 8.0,
+                      ),
+                      Image.asset("assets/images/raro_academy_logo.png"),
+                      SizedBox(height: 40.0)
+                    ],
+                  ),
                 ),
               )
             ],
