082fb9baa34b00fb19f42dac6b9fe7e22272f277
Airton Araujo <airton.araujo.s18@gmail.com>
2021-07-18 23:52:24 -0300

    note screen finalized

--- lib/src/modules/notes/notes_page.dart
+++ lib/src/modules/notes/notes_page.dart
@@ -3,6 +3,7 @@ import 'package:bloco_de_notas/src/shared/constants/app_colors.dart';
 import 'package:bloco_de_notas/src/shared/widgets/card_note.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class NotesPage extends StatefulWidget {
   @override
@@ -10,6 +11,38 @@ class NotesPage extends StatefulWidget {
 }
 
 class _NotesPageState extends State<NotesPage> {
+  List<CardNote> cardNoteList = [
+    CardNote(
+        'Não esquecer',
+        'Lorem ipsum dolor sit amet, consecter adipiscing elit, sed incididunt ut labore et dolore aliqua.',
+        'Criação: 08/07/21',
+        AppColors.rosa),
+    CardNote(
+        'Fazer compras',
+        'Lorem ipsum dolor sit amet, consecter adipiscing elit, sed incididunt ut labore et dolore aliqua., consecter adipiscing elit, sed incididunt ut labore et dolore aliqua., adasfa ',
+        'Criação: 08/07/21',
+        AppColors.roxo),
+    CardNote(
+        'Estudar Flutter',
+        'Lorem ipsum dolor testatando que o flutter é muito bom vamos que vamos sit amet, consecter adipiscing elit, sed incididunt ut labore et dolore aliqua.',
+        'Criação: 08/07/21',
+        AppColors.amarelo),
+    CardNote(
+        'Lembrete para ir ao médico',
+        'Lorem ipsum dolor sit amet, consecter adipiscing elit, sed incididunt ut labore et dolore aliqua.',
+        'Criação: 08/07/21',
+        AppColors.verde),
+    CardNote(
+        'Nova feature APP Flutter',
+        'Lorem ipsum dolor sit amet, consecter adipiscing elit, sed incididunt ut labore et dolore aliqua.',
+        'Criação: 08/07/21',
+        AppColors.ciano),
+    CardNote(
+        'Contas',
+        'Lorem ipsum dolor sit amet, consecter adipiscing elit, sed incididunt ut labore et dolore aliqua.',
+        'Criação: 08/07/21',
+        AppColors.rosa),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,14 +59,14 @@ class _NotesPageState extends State<NotesPage> {
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 28),
+            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 40),
             child: TextField(
               decoration: InputDecoration(
                 suffixIcon: Icon(Icons.search),
                 hintText: "Pesquisar",
                 hintStyle: TextStyle(
                   fontFamily: "Roboto",
-                  fontWeight: FontWeight.w700,
+                  fontWeight: FontWeight.w400,
                   fontSize: 16.0,
                   letterSpacing: 0.15,
                   color: Colors.black.withOpacity(0.54),
@@ -54,8 +87,21 @@ class _NotesPageState extends State<NotesPage> {
               ),
             ),
           ),
-          CardNote()
-          // GridView.builder(gridDelegate: , itemBuilder: itemBuilder)
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: StaggeredGridView.count(
+                crossAxisCount: 4,
+                padding: const EdgeInsets.all(2.0),
+                children: cardNoteList,
+                staggeredTiles: cardNoteList
+                    .map<StaggeredTile>((_) => StaggeredTile.fit(2))
+                    .toList(),
+                mainAxisSpacing: 6.0,
+                crossAxisSpacing: 6.0,
+              ),
+            ),
+          )
         ],
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
--- lib/src/shared/widgets/card_note.dart
+++ lib/src/shared/widgets/card_note.dart
@@ -2,6 +2,12 @@ import 'package:bloco_de_notas/src/shared/constants/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class CardNote extends StatelessWidget {
+  final String title;
+  final String content;
+  final String dateCreation;
+  final Color titleColor;
+
+  CardNote(this.title, this.content, this.dateCreation, this.titleColor);
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -17,7 +23,7 @@ class CardNote extends StatelessWidget {
             Container(
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(4),
-                color: AppColors.rosa,
+                color: titleColor,
               ),
               width: double.maxFinite,
               child: Column(
@@ -26,7 +32,7 @@ class CardNote extends StatelessWidget {
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Text(
-                      'Não esquecer',
+                      title,
                       style: TextStyle(
                           fontFamily: 'Roboto',
                           color: Colors.white,
@@ -76,7 +82,7 @@ class CardNote extends StatelessWidget {
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Text(
-                      'Lorem ipsum dolor sit amet, consecter adipiscing elit, sed incididunt ut labore et dolore aliqua.',
+                      content,
                       style: TextStyle(
                           fontFamily: 'Roboto',
                           color: Color.fromRGBO(0, 0, 0, 0.54),
@@ -95,7 +101,7 @@ class CardNote extends StatelessWidget {
                   Padding(
                     padding:
                         const EdgeInsets.only(bottom: 8, left: 8, right: 8),
-                    child: Text('Criação: 08/07/21',
+                    child: Text(dateCreation,
                         style: TextStyle(
                             fontStyle: FontStyle.italic,
                             fontFamily: 'Roboto',
